f04a611c42cc1f9b82a84fcb727274b21055dd19
entom <tomasz@entom.info>
2021-03-01 17:40:35 +0100

    refactor(app): install and configure security package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const fastify = require('fastify')
+const helmet = require('fastify-helmet');
 
 function build(opts = {}) {
   const app = fastify(opts)
@@ -10,6 +11,11 @@ function build(opts = {}) {
       ejs: require('ejs')
     }
   })
+
+  app.register(helmet, {
+    contentSecurityPolicy: false
+  })
+
 
   app.register(require('./routes/index'))
   app.register(require('./routes/api/status'), {prefix: '/api'})
